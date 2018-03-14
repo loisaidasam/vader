9c701d66cb69f452aba6bc60c7916de3c7284165
LoisaidaSam <sam.sandberg@gmail.com>
2018-03-13 21:24:51 -0400

    button-test.py

---
+++ button-test.py
@@ -0,0 +1,23 @@
+# Button click
+# via https://www.hackster.io/hardikrathod/push-button-with-raspberry-pi-6b6928
+
+import RPi.GPIO as GPIO
+import time
+
+PIN_READ = 23
+
+GPIO.setmode(GPIO.BCM)
+
+# Button to GPIO23
+GPIO.setup(PIN_READ, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        button_state = GPIO.input(23)
+        if button_state:
+            print('Button Pressed...')
+        else:
+            print('Button Pressed...')
+        time.sleep(0.2)
+except:
+    GPIO.cleanup()

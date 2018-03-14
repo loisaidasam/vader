ba294bbc3f58e973773af516dc9e20da1b508a0b
LoisaidaSam <sam.sandberg@gmail.com>
2018-03-13 22:32:38 -0400

    servo-test-pigpio.py

---
+++ servo-test-pigpio.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# via https://raspberrypiwonderland.wordpress.com/2014/02/19/servo-test/
+
+import time
+
+import pigpio
+
+# GPIO number
+servos = 18
+
+pigpio.start()
+# pulsewidth can only set between 500-2500
+
+try:
+    while True:
+        # 0 degree
+        pigpio.set_servo_pulsewidth(servos, 500)
+        print("Servo {} {} micro pulses".format(servos, 1000))
+        time.sleep(1)
+        # 90 degree
+        pigpio.set_servo_pulsewidth(servos, 1500)
+        print("Servo {} {} micro pulses".format(servos, 1500))
+        time.sleep(1)
+        # 180 degree
+        pigpio.set_servo_pulsewidth(servos, 2500)
+        print("Servo {} {} micro pulses".format(servos, 2000))
+        time.sleep(1)
+        # 180 degree
+        pigpio.set_servo_pulsewidth(servos, 1500)
+        print("Servo {} {} micro pulses".format(servos, 1500))
+        time.sleep(1)
+
+# switch all servos off
+except KeyboardInterrupt:
+    pigpio.set_servo_pulsewidth(servos, 0)
+
+pigpio.stop()

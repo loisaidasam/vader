1a3a1fd47586c3983782921e32059e412c7068b2
LoisaidaSam <sam.sandberg@gmail.com>
2018-03-13 22:55:41 -0400

    Test pigpio range

--- servo-test-pigpio.py
+++ servo-test-pigpio.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 
-# https://raspberrypiwonderland.wordpress.com/2014/02/19/servo-test/
-# http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
-# http://abyz.me.uk/rpi/pigpio/index.html
-# https://elinux.org/RPi_GPIO_Code_Samples#pigpio_2
+"""
+The selected pulsewidth will continue to be transmitted until changed by a
+subsequent call to set_servo_pulsewidth.
+
+The pulsewidths supported by servos varies and should probably be determined by
+experiment. A value of 1500 should always be safe and represents the mid-point
+of rotation.
+
+You can DAMAGE a servo if you command it to move beyond its limits.
+
+Example
+
+pi.set_servo_pulsewidth(17, 0)    # off
+pi.set_servo_pulsewidth(17, 1000) # safe anti-clockwise
+pi.set_servo_pulsewidth(17, 1500) # centre
+pi.set_servo_pulsewidth(17, 2000) # safe clockwise
+
+https://raspberrypiwonderland.wordpress.com/2014/02/19/servo-test/
+http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
+http://abyz.me.uk/rpi/pigpio/index.html
+https://elinux.org/RPi_GPIO_Code_Samples#pigpio_2
+"""
 
 import time
 
@@ -12,45 +30,27 @@ import pigpio
 # GPIO number
 PIN = 18
 
+MIN = 1000
+MAX = 2000
+
+STEP = 5
+
+DELAY = 0.05
+
 pi = pigpio.pi()
 # pulsewidth can only set between 500-2500
 
 try:
     pi.set_mode(PIN, pigpio.OUTPUT)
-    pi.set_servo_pulsewidth(PIN, 1500)
-    time.sleep(5)
-    pi.set_servo_pulsewidth(PIN, 1000)
-    time.sleep(5)
-    pi.set_servo_pulsewidth(PIN, 1500)
-    time.sleep(5)
-    #pi.set_servo_pulsewidth(PIN, 0)
+    for pulse in range(MIN, MAX, STEP):
+        print pulse
+        pi.set_servo_pulsewidth(PIN, pulse)
+        time.sleep(DELAY)
+    for pulse in range(MAX, MIN, -1 * STEP):
+        print pulse
+        pi.set_servo_pulsewidth(PIN, pulse)
+        time.sleep(DELAY)
+except KeyboardInterrupt:
+    pi.set_servo_pulsewidth(PIN, 0)
 finally:
     pi.stop()
-
-
-'''
-try:
-    while True:
-        # 0 degree
-        pi.set_servo_pulsewidth(servos, 500)
-        print("Servo {} {} micro pulses".format(servos, 1000))
-        time.sleep(1)
-        # 90 degree
-        pigpio.set_servo_pulsewidth(servos, 1500)
-        print("Servo {} {} micro pulses".format(servos, 1500))
-        time.sleep(1)
-        # 180 degree
-        pigpio.set_servo_pulsewidth(servos, 2500)
-        print("Servo {} {} micro pulses".format(servos, 2000))
-        time.sleep(1)
-        # 180 degree
-        pigpio.set_servo_pulsewidth(servos, 1500)
-        print("Servo {} {} micro pulses".format(servos, 1500))
-        time.sleep(1)
-
-# switch all servos off
-except KeyboardInterrupt:
-    pigpio.set_servo_pulsewidth(servos, 0)
-
-pigpio.stop()
-'''

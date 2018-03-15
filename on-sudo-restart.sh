31fe14f433a66805501a9904b3c6f0125821db8f
sam@c3po <sam.sandberg@gmail.com>
2018-03-14 22:55:48 -0400

    A script for sudo's crontab `@restart`

---
+++ on-sudo-restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start pigpiod
+/usr/bin/pigpiod 
+
+# Set the volume
+/home/sam/source/vader/bin_home/volume_default.sh
+
+# Play a welcome sound
+/usr/bin/aplay /home/sam/source/vader/sounds/proud.wav
+
+# Kick it off
+python /home/sam/source/vader/vader.py > /home/sam/source/vader/logs/vader.log 2>&1
+
